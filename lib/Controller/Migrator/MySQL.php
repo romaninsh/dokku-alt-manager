be78bae5fc3d05c122c0e46b2c62c6e62503da4f
NicolasRitouet <nicolas@ritouet.com>
2015-06-08 22:42:15 +0200

    Move lib folder to root

---
+++ lib/Controller/Migrator/MySQL.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Implements a generic migration controller.
+ */
+class Controller_Migrator_MySQL extends AbstractController {
+
+    public $db=null;
+    function migrate(){
+        // find migrations
+
+        if(is_null($this->db))$this->db=$this->app->db;
+        if(!$this->db)$this->db = $this->app->dbConnect();
+
+        $folders = $this->app->pathfinder->search('dbupdates');
+
+        $this->db->dsql()->expr('create table if not exists `[table]` '.
+            '(id int not null primary key auto_increment, name varchar(255), unique key(name), status enum("ok","fail"))',
+            ['table'=>'_db_update'])->execute();
+
+        // todo - sort files in folders
+        foreach($folders as $dir){
+
+            $files = scandir($dir);
+            sort($files);
+            foreach($files as $name) {
+                if(strtolower(substr($name,-4))!='.sql')continue;
+
+                $q = $this->db->dsql()
+                    ->table('_db_update')
+                    ->where('name',strtolower($name))
+                    ->field('status');
+
+                if($q->getOne()==='ok')continue;
+
+                $migration=file_get_contents($dir.'/'.$name);
+
+                $q->set('name',strtolower($name));
+                try {
+                    $this->db->dbh->exec($migration);
+                    $q->set('status','ok')->replace();
+                }catch(Exception $e){
+                    $q->set('status','fail')->replace();
+                    if(!$e instanceof BaseException){
+                        $e = $this->exception()
+                            ->addMoreInfo('Original error', $e->getMessage());
+                    }
+                    throw $e->addMoreInfo('file',$name);
+                }
+            }
+        }
+    }
+
+}
